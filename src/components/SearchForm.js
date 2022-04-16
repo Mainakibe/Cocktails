6d19e8e7b2acb61b049f76456117c9bcfe28c28f
Maina Kibe <mainakibe@outlook.com>
2022-04-16 23:52:35 +0300

    third commit

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
-  const { searchItems } = useGlobalContext();
+  const { setSearchItems } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchItems(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <>
-      <form className="search-form">
+      <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="name">search your favorite cocktail</label>
           <input
             type="text"
             name="name"
             id="name"
-            value={searchItems}
+            ref={searchValue}
+            onChange={searchCocktail}
           />
         </div>
       </form>
--- src/context.js
+++ src/context.js
@@ -6,12 +6,43 @@ const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
   const [searchItems, setSearchItems] = useState('b');
-  const [searchResults, setSearchResults] = useState([]);
+  const [cocktails, setCocktails] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {});
+  const getDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`${url}${searchItems}`);
+      const data = await res.json();
+      const { drinks } = data;
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+        setCocktails(newCocktails);
+      } else {
+        setCocktails([]);
+      }
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  }, [searchItems]);
+
+  useEffect(() => {
+    getDrinks();
+  }, [searchItems, getDrinks]);
   return (
-    <AppContext.Provider value={{ searchItems, loading }}>
+    <AppContext.Provider value={{ setSearchItems, loading, cocktails }}>
       {children}
     </AppContext.Provider>
   );
